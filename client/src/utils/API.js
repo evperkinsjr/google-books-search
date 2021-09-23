a08be43d5266ec30bfc26140edf5900a281fed3e
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-23 15:17:07 -0400

    created and exported API methods

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,23 @@
 import axios from "axios";
+
+export default {
+    // Get books from Google Books Search API
+    getBooks: function(query) {
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=", { params: { q: query } });
+    },
+
+    // Get saved books from the database
+    getSavedBooks: function() {
+        return axios.get("/api/books");
+    },
+
+    // Saves a book to the database
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+
+    // Deletes a book from the database
+    deleteBook: function(id) {
+        return axios.delete("/api/books" + id);
+    }
+};

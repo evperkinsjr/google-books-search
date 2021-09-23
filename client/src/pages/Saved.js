8e3fa5620797f428c4cd774a0afb92db0758dd19
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-23 16:01:21 -0400

    coded and exported Saved page

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import { BookList, BookListItem } from "../components/BookList";
+import { Container, Row, Col } from "../components/Grid";
+
+function Saved() {
+    // Setting our component's initial state
+    const [savedBooks, setSavedBooks] = useState([])
+
+    // Load all books and store them with setSavedBooks
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    // Loads all books and sets them to books
+    function loadBooks() {
+        API.getSavedBooks()
+            .then(res =>
+                setSavedBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
+    // Deletes a book, then reloads saved books
+    const handleDeleteBook = event => {
+        event.preventDefault();
+
+        API.deleteBook(event.target.id)
+            .then(res => loadBooks())
+            .catch(err => console.log(err));
+    }
+
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        {!savedBooks.length ? (
+                            <h2 className="text-center">No Bookds to Display</h2>
+                        ) : (
+                            <BookList type="Saved Books">
+                                {savedBooks.map(book => {
+                                    return (
+                                        <BookListItem
+                                            key={book._id}
+                                            thumbnail={book.thumbnail}
+                                            title={book.title}
+                                            authors={book.authors}
+                                            description={book.description}
+                                            link={book.link}
+                                            index={book._id}
+                                            onClick={handleDeleteBook}
+                                            buttonName="Delete"
+                                        />  
+                                    );
+                                })}
+                            </BookList>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Saved;

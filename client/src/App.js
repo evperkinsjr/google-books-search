a28969b9c73cec051363c2c59b88267df1ec93ff
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-23 17:29:12 -0400

    added components and pages to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import NoMatch from "./pages/NoMatch";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Jumbotron />
+      <Switch>
+        <Route exact path="/">
+          <Search />
+        </Route>
+        <Route exact path="/saved">
+          <Saved />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

7153260a0536aa7e562159f50f635234177ff86f
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-24 13:03:19 -0400

    modified id field name

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    _id: { type: String, required: true, unique: true },
+    id: { type: String, required: true, unique: true },
     title: { type: String, required: true },
     authors: { type: Array },
     description: { type: String },

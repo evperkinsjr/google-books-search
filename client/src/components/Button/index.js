78903ec8ef249a5950e3423db6407afc29ccc899
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-24 13:06:11 -0400

    added id to button component

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Button({ type, className, children, onClick}) {
+function Button({ id, type, className, children, onClick}) {
     return (
-        <button onClick={onClick} className={["btn btn-lg", `btn-${type}`, className].join(" ")}>
+        <button id={id} onClick={onClick} className={["btn btn-lg", `btn-${type}`, className].join(" ")}>
             {children}
         </button>
     );

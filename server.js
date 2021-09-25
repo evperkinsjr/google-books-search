bef5549cef409cc8562b325c8e4e6806d16f01d7
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-25 14:51:33 -0400

    deploying

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ if (process.env.NODE_ENV === "production") {
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   });
 
 // Define API routes here
